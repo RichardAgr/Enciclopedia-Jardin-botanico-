f7ec8a951bdcba51b5da77750f43316972637cba
Ricardo <richardyord@gmail.com>
2023-11-14 22:33:16 +0100

    Creacion de header y eliminacion de archivos

---
+++ arquiBotanico/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css'
+
+import Header from './components/menuNavegacion/header'
+
+function App() {
+  return (
+      <div className="App">
+         <Header />
+         {/* <MenuNav />
+         <Footer />  */}
+      </div>
+  )
+}
+
+export default App
---
+++ arquiBotanico/src/components/menuNavegacion/header.jsx
@@ -0,0 +1,10 @@
+import "./header.css"
+
+const header = () => {
+    return <div class= "headerA">
+        <h1>Logo</h1>
+        <img class="imgB" src="/src/assets/banerr.jpeg" alt="logo" />
+    </div>
+}
+
+export default header;
