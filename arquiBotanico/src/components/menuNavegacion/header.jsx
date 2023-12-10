cea296a312b85cfa6fef0d17508e0fe9233fce16
Juan Pablo <juanpablojustinianoorosco@gmail.com>
2023-12-10 14:00:15 -0400

    Elimino el formato

--- arquiBotanico/src/components/menuNavegacion/header.jsx
+++
@@ -1,9 +0,0 @@
-import "./header.css"
-
-const header = () => {
-    return <div class= "headerA">
-        <img class="imgB" src="/src/assets/jardinBotanico.png" alt="logo" />
-    </div>
-}
-
-export default header;
--- arquiBotanico/src/components/navegacion/headerNav.jsx
+++
@@ -1,7 +0,0 @@
-import React, { useState } from 'react';
-
-const App2 = () => {
-    return(
-        <h1>sdfas</h1>
-    );
-};
